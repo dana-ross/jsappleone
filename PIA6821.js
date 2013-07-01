f55f8af79805a386c479f8b5e39b2f014c29ce40
Dave Ross <dave@davidmichaelross.com>
2013-07-01 02:19:27 -0400

    fleshing out the PIA some more

--- PIA6821.js
+++ PIA6821.js
@@ -2,14 +2,17 @@ function PIA6821(emulator) {
 
 	this.CRA = 0; this.PAI = 0; this.PAO = 0; this.DDRA = 0;
 	this.CRB = 0; this.PBI = 0; this.PBO = 0; this.DDRB = 0;
+	this.emulator = emulator;
 
 	this.read = function(rs) {
 		switch(rs) {
 			case 0:
+				this.CRA = this.CRA & 127;
 				return (this.CRA & 3) ? (this.PAI & ~this.DDRA) : this.DDRA;
 			case 1:
 				return this.CRA;
 			case 2:
+				this.CRB = this.CRB & 127;
 				return (this.CRB & 3) ? (this.PBI & ~this.DDRB) : this.DDRB;
 			case 3:
 				return this.CRB;
@@ -19,13 +22,32 @@ function PIA6821(emulator) {
 	this.write = function(rs, value) {
 		switch(rs) {
 			case 0:
+				this.PAO = value;
+				break;
 			case 1:
 				this.CRA = value & 63;
 				break;
 			case 2:
+				this.PBO = value;
+				break;
 			case 3:
 				this.CRB = value & 63;
 				break;
 		}
 	};
+
+	this.trigger_ca1 = function() {
+		this.CRA = this.CRA | 128;
+		if(this.CRA & 1) {
+			this.emulator.cpu.irq();
+		}
+	};
+
+	this.output_a = function() {
+		return this.PAO & this.DDRA;
+	}
+
+	this.output_b = function() {
+		return this.PBO & this.DDRB;
+	}
 }
